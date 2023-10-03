2e7ef1f0979bd518cc017c43bc0bc175e74e1dae
syedimran786 <syedimranhgmrd@gmail.com>
2023-10-03 12:25:57 +0530

    tasks updated

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -7,9 +7,12 @@ let addTask=async (req,res,next)=>
     {
         let {userId,tname}=req.body;
         console.log("userId")
-        userId=req.user._id;
+        // userId=req.user._id;
 
+        // let task=await Task.create({userId,tname});
         let task=await Task.create({userId,tname});
+
+        
 
         if(task)
         {
@@ -23,6 +26,91 @@ let addTask=async (req,res,next)=>
     }
 }
 
+
+let getAllTask=async (req,res,next)=>
+{
+    try
+    {
+       
+        let task=await Task.find();
+
+        if(!task.length)
+        {
+            return res.status(404).json({error:true,message:"No Task Available",data:task})
+        }
+        return res.status(200).json({error:false,message:"Tasks fetched Successfully",data:task})
+    }
+    catch(err)
+    {
+        next(err)
+    }
+}
+
+let getSingleTask=async (req,res,next)=>
+{
+    try
+    {
+      
+     
+        let task=await Task.findOne({_id:req.params.id});
+
+        if(task)
+        {
+            return res.status(200).json({error:false,message:"Task Fetched Successfully",data:task})
+        }
+        return res.status(404).json({error:true,message:"No Task Found"})
+    }
+    catch(err)
+    {
+        next(err)
+    }
+}
+
+let updateTask=async (req,res,next)=>
+{
+    try
+    {
+      let isTaskAvailable=await Task.findOne({_id:req.params.id})
+
+      if(!isTaskAvailable)
+      {
+        return res.status(404).json({error:true,message:"No Task Found"})
+      }
+     
+        let task=await Task.findOneAndUpdate({_id:req.params.id},{$set:req.body},{new:true,runValidators:true});
+        return res.status(200).json({error:false,message:"Task Updated Successfully",data:task})   
+    }
+    catch(err)
+    {
+        next(err)
+    }
+}
+
+let deleteTask=async (req,res,next)=>
+{
+    try
+    {
+      let isTaskAvailable=await Task.findOne({_id:req.params.id})
+
+      if(!isTaskAvailable)
+      {
+        return res.status(404).json({error:true,message:"No Task Found"})
+      }
+     
+        let task=await Task.deleteOne({_id:req.params.id});
+        return res.status(200).json({error:false,message:"Task Deleted Successfully"})   
+    }
+    catch(err)
+    {
+        next(err)
+    }
+}
+
+
 module.exports={
-    addTask
+    addTask,
+    getAllTask,
+    getSingleTask,
+    updateTask,
+    deleteTask
 }
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { addTask } = require('../controllers/task.controller');
+const { addTask, getAllTask,
+    getSingleTask,
+    updateTask,
+    deleteTask } = require('../controllers/task.controller');
 const {auth} = require('../services/authService')
 
 let router=express.Router();
 
-router.post("/addtask",auth,addTask);
+router.post("/addtask",addTask);
+router.get("/tasks",getAllTask);
+router.get("/task/:id",getSingleTask);
+router.put("/updatetask/:id",updateTask);
+router.delete("/deletetask/:id",deleteTask);
+
 
 module.exports=router
